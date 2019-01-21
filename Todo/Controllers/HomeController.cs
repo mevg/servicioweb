54891fcef86842e43b726d3e3d779a5dd7fc8526
Eduardo Valenzuela <lalo.valenz.glez@gmail.com>
2019-01-20 17:38:03 -0700

    servicios web prueba

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Todo.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private List<Models.Todo> list = new List<Models.Todo>();
+
+        public HomeController()
+        {
+            list.Add(new Models.Todo()
+            {
+                id = 1,
+                name = "compra caguamas"
+            });
+            list.Add(new Models.Todo()
+            {
+                id = 2,
+                name = "compra mas caguamas"
+            });
+        }
+        public IHttpActionResult Get()
+        {
+            //buscar clientes
+            //buscar otra cosa
+            //    mandar json con las dos variables
+            return Ok(list);
+        }
+
+        public IHttpActionResult Get(int num)
+        {
+            //Created( un reporte de nose que qcosa)
+            return Ok($"tu numero gay es {num}");
+        }
+    }
+}
---
+++ Todo/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Todo.Models
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
